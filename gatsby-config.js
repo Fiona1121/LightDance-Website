b8893b727005a2f30aeca764cd77b6914ef04f3b
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-01-24 15:57:29 +0800

    Initialize with Gatsby and contentful

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+      title: `NTUEE LIGHTDANCE`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "nxtQ1jVoLrYFPaSuFlvhkigI2RW5E7GgsTevwQzYOsU",
+      "spaceId": "ylwybf063k0n"
+    }
+  }]
+};
