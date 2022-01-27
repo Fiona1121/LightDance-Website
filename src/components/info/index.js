d7b4159309dacc1bcee36064a1adb8105057d6cc
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-01-27 15:52:46 +0800

    fix info page

--- src/components/info/index.js
+++ src/components/info/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import "./index.css";
@@ -22,24 +22,28 @@ const Info = () => {
             <div className="title">Contact Us!</div>
             <div className="wrapper">
                 <div className="icon-container">
-                    <div className="icon email" href={data.filter((link) => link.includes("gmail"))[0]}>
+                    <div className="icon email">
                         <div className="tooltip">Email</div>
-                        <span>
+                        <button
+                            onClick={() => {
+                                window.open(`mailto:${data.filter((link) => link.includes("gmail"))[0]}`);
+                            }}
+                        >
                             <FontAwesomeIcon className="fab fa-envelope" icon={faEnvelope}></FontAwesomeIcon>
-                        </span>
+                        </button>
                     </div>
 
-                    <div className="icon github" href={data.filter((link) => link.includes("github"))[0]}>
+                    <div className="icon github">
                         <div className="tooltip">Github</div>
-                        <span>
+                        <Link to={data.filter((link) => link.includes("github"))[0]}>
                             <FontAwesomeIcon className="fab fa-github" icon={faGithub}></FontAwesomeIcon>
-                        </span>
+                        </Link>
                     </div>
-                    <div className="icon youtube" href={data.filter((link) => link.includes("youtube"))[0]}>
+                    <div className="icon youtube">
                         <div className="tooltip">Youtube</div>
-                        <span>
+                        <Link to={data.filter((link) => link.includes("youtube"))[0]}>
                             <FontAwesomeIcon className="fab fa-youtube" icon={faYoutube}></FontAwesomeIcon>
-                        </span>
+                        </Link>
                     </div>
                 </div>
             </div>
